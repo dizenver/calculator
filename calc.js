121422195be71243288e5db420c5e219da727a1f
Denver Martinez <denver.martinez@gmail.com>
2022-06-01 00:53:24 -0500

    Add Calculator functions

--- calc.js
+++ calc.js
@@ -1,3 +1,96 @@
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetDisplay = false
+
+const calcNumbers = document.querySelectorAll('.calc-numbers')
+const calcOperators = document.querySelectorAll('[data-operator]')
+const equalsBtn = document.getElementById('equals')
+const clearBtn = document.getElementById('clear')
+const deleteBtn = document.getElementById('delete')
+const plusMinusBtn = document.getElementById('plusminus')
+const decimalBtn = document.getElementById('decimal')
+
+const equationDisplay = document.getElementById('equation')
+const answerDisplay = document.getElementById('answer')
+
+equalsBtn.addEventListener('click', evaluate)
+clearBtn.addEventListener('click', clear)
+deleteBtn.addEventListener('click', deleteNum)
+decimalBtn.addEventListener('click', appendPoint)
+plusMinusBtn.addEventListener('click', plusMinus)
+
+calcNumbers.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+calcOperators.forEach((button) =>
+    button.addEventListener('click', () => setOperator(button.textContent))
+)
+
+function appendNumber(num) {
+    if(answerDisplay.textContent ===  '0'  || shouldResetDisplay)
+        resetDisplay()
+        answerDisplay.textContent += num
+}
+
+function resetDisplay() {
+    answerDisplay.textContent = ''
+    shouldResetDisplay = false
+}
+
+function clear() {
+    answerDisplay.textContent = '0'
+    equationDisplay.textContent = ''
+    firstOperand = ''
+    secondOperand = ''
+    currentOperation = null
+}
+
+function appendPoint() {
+    if (shouldResetDisplay) resetDisplay()
+    if (answerDisplay.textContent === '')
+      answerDisplay.textContent = '0'
+    if (answerDisplay.textContent.includes('.')) return
+    answerDisplay.textContent += '.'
+  }
+
+function deleteNum() {
+    answerDisplay.textContent = answerDisplay.textContent.toString().slice(0, -1)
+}
+
+function plusMinus() {
+    let value = answerDisplay.textContent
+    answerDisplay.textContent = value * -1;
+}
+
+function setOperator(operator) {
+    if(currentOperation !== null) evaluate()
+    firstOperand  = answerDisplay.textContent
+    currentOperation = operator
+    equationDisplay.textContent = `${firstOperand} ${currentOperation}`
+    shouldResetDisplay = true
+}
+
+function evaluate () {
+    if (currentOperation === null || shouldResetDisplay) return
+    if(currentOperation === '+' && answerDisplay.textContent === '0') {
+        alert ("You can't divide by 0!")
+        return
+    }
+    secondOperand = answerDisplay.textContent
+    answerDisplay.textContent = roundAnswer (
+        operate(currentOperation, firstOperand, secondOperand)
+    )
+    equationDisplay.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+    currentOperation = null
+}
+
+function roundAnswer(num) {
+    return Math.round(num * 1000) / 1000
+}
+
+
 /* Basic Calc Functions */
 function add(a,b) {
     sum = a + b;
@@ -18,23 +111,23 @@ function divide(a,b) {
     return sum;
 };
 
-function operate(operator,a,b) {
-    operator(a,b)
-    return sum;
-};
 
 
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
 
-/* Event Listener for Buttons */
-const calcButtons = document.querySelectorAll('.calc-buttons');
-
-calcButtons.forEach(button => {
-  button.addEventListener('click', function handleClick(event) {
-    console.log('box clicked', event);
-    button.setAttribute('style', 'background-color: yellow;');
-
-    /*display button pressed */
-    let display = document.getElementById('display');
-    display.textContent = button.id;
-  });
-});
+    switch (operator) {
+      case '+':
+        return add(a, b)
+      case '-':
+        return subtract(a, b)
+      case 'x':
+        return multiply(a, b)
+      case '÷':
+        if (b === 0) return null
+        else return divide(a, b)
+      default:
+        return null
+    }
+  }
